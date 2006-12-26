a58f5fd210fe5bc8fed3e1f49c7cf76771043913
ramr <ramr>
2006-12-26 02:13:58 +0000

    Now includes ldapauth_global.h.

--- ldapauth.h
+++ ldapauth.h
@@ -20,48 +20,16 @@
 
 #ifndef _IISLDAPAUTH_H_
 #define _IISLDAPAUTH_H_
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+
 #include <windows.h>
 #include <httpfilt.h>
-#include "string_safe.h"
+#include "ldapauth_global.h"
 
 /*
     Constants
 */
-#define MAXSTRLEN			1024
 #define MODULE_CONF_FILE	"\\ldapauth.ini"	/*  Include beginning backslash  */
 #define USER_SEARCH_KEY		"%username%"
-
-/*
-	Compile Options
-*/
-#define IISLDAPAUTH_DEBUG					1
-#define IISLDAPAUTH_DENYBLANKPASSWORDS		1
-#define IISLDAPAUTH_CACHE					1
-#define IISLDAPAUTH_FILE_LOG				1
-/*
-#define IISLDAPAUTH_BSTENTERPRISEHACK		1
-*/
-
-/*
-
-	Visual Studio 2005 includes support for strlcpy() and strlcat().
-	Enable the #define to turn off our versions of these routines.
-
-#define VS2005				1
-*/
-
-/*
-	Debug Strings
-*/
-#ifdef IISLDAPAUTH_FILE_LOG
-#define DebugWrite( x ) Log_Write( x, LDAPLOG_DEBUG )
-#else
-#define DebugWrite( x )      /* nothing */
-#endif /* IISLDAPAUTH_FILE_LOG */
-
 
 /*
 	Log entry cache
@@ -138,12 +106,5 @@ LDAPDB_Terminate(
 #ifdef IISLDAPAUTH_CACHE
 #include "cache.h"
 #endif  /* IISLDAPAUTH_CACHE */
-
-/*
-	Logging routines
-*/
-#ifdef IISLDAPAUTH_FILE_LOG
-#include "ldapauthlog.h"
-#endif  /* IISLDAPAUTH_FILE_LOG */
 
 #endif /* _IISLDAPAUTH_H_ */
